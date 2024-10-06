80da5c259ec51ed26bf81ec6f7bacc4f8371a833
AAYUSHI GUPTA <134951518+gupta-aayushi@users.noreply.github.com>
2024-10-07 00:26:11 +0530

    Time: 2 ms (72.13%), Space: 41.3 MB (90.28%) - LeetHub

--- 1960-check-if-the-sentence-is-pangram/1960-check-if-the-sentence-is-pangram.java
+++ 1960-check-if-the-sentence-is-pangram/1960-check-if-the-sentence-is-pangram.java
@@ -1,12 +1,22 @@
 class Solution {
     public boolean checkIfPangram(String sentence) {
-        HashMap <Character,Integer> map = new HashMap<>();
+        /*HashMap <Character,Integer> map = new HashMap<>();
         for (int i=0; i<sentence.length(); i++) {
             map.put(sentence.charAt(i), map.getOrDefault(sentence.charAt(i),0)+1);
         }
         if(map.size()==26) {
             return true;
         }
-        return false;
+        return false;*/
+        int[] arr = new int[26];
+        for (int i=0; i<sentence.length(); i++) {
+            arr[sentence.charAt(i)-'a']++;
+        }
+        for (int i=0; i<arr.length; i++) {
+            if(arr[i]<1) {
+                return false;
+            }
+        }
+        return true;
     }
 }
