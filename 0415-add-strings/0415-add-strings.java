fc489492795421d48b3592f20f0950a6cd2e9967
AAYUSHI GUPTA <134951518+gupta-aayushi@users.noreply.github.com>
2024-10-13 23:50:39 +0530

    Time: 1 ms (100%), Space: 42.7 MB (48.18%) - LeetHub

--- 0415-add-strings/0415-add-strings.java
+++ 0415-add-strings/0415-add-strings.java
@@ -5,12 +5,10 @@ class Solution {
         while (i >= 0 || j >= 0 || carry > 0) {
             int digit1 = (i >= 0) ? num1.charAt(i--) - '0' : 0;
             int digit2 = (j >= 0) ? num2.charAt(j--) - '0' : 0;
-
             sum = digit1 + digit2 + carry;
             str.append(sum % 10);
             carry = sum / 10;
         }
-
         return str.reverse().toString();
     }
 }
