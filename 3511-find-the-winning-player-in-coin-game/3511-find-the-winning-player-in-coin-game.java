54f92adbffaacd470659c124041cdd6cf0410ffb
AAYUSHI GUPTA <134951518+gupta-aayushi@users.noreply.github.com>
2025-08-27 12:39:05 +0530

    Time: 1 ms (23.48%), Space: 41.9 MB (33.33%) - LeetHub

---
+++ 3511-find-the-winning-player-in-coin-game/3511-find-the-winning-player-in-coin-game.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String winningPlayer(int x, int y) {
+        int count = 0;
+        while (x>=1 && y>=4){
+            x = x - 1;
+            y = y - 4;
+            count++;
+        }
+        if (count%2==0){
+            return "Bob";
+        }
+        else {
+            return "Alice";
+        }
+    }
+}
