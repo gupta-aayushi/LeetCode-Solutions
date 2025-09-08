de33396dd50d82797ce10cc5b00391f995eda233
AAYUSHI GUPTA <134951518+gupta-aayushi@users.noreply.github.com>
2025-09-08 20:43:08 +0530

    Time: 0 ms (100%), Space: 40.9 MB (62.24%) - LeetHub

--- 3817-maximum-containers-on-a-ship/3817-maximum-containers-on-a-ship.java
+++ 3817-maximum-containers-on-a-ship/3817-maximum-containers-on-a-ship.java
@@ -1,13 +1,5 @@
 class Solution {
     public int maxContainers(int n, int w, int maxWeight) {
-        int n2 = n*n;
-        int i = 0;
-        int count = 0;
-        while (maxWeight>=w && i<n2){
-            count++;
-            i++;
-            maxWeight -= w;
-        }
-        return count;
+        return Math.min(n*n, maxWeight/w);
     }
 }
