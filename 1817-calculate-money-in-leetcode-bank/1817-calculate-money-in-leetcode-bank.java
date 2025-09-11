cd75be50e92e31ff45e5dcb0fa23cc30c989a9c0
AAYUSHI GUPTA <134951518+gupta-aayushi@users.noreply.github.com>
2025-09-11 12:44:35 +0530

    Time: 0 ms (100%), Space: 40.8 MB (16.36%) - LeetHub

--- 1817-calculate-money-in-leetcode-bank/1817-calculate-money-in-leetcode-bank.java
+++ 1817-calculate-money-in-leetcode-bank/1817-calculate-money-in-leetcode-bank.java
@@ -1,18 +1,10 @@
 class Solution {
     public int totalMoney(int n) {
-        int moncount = 0;
-        int sum = 0;
-        for (int i=1; i<=n; i++){
-            if (i%7==1){
-                moncount++;
-                sum += moncount;
-            }
-            else if (i%7==0) {
-                sum += 7 + moncount - 1;
-            }
-            else {
-                sum += i%7 + moncount - 1;
-            }
+        int weeks = n/7;
+        int days = n%7;
+        int sum = weeks * 28 + (weeks * (weeks - 1) / 2) * 7;
+        for (int i = 1; i <= days; i++) {
+            sum += weeks + i;
         }
         return sum;
     }
